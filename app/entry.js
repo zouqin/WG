190dcbc53bbccd7a83d97ea9a16803983033a70e
zouqin <zouqin3306@sina.com>
2017-04-19 16:03:50 +0800

    react入口文件

---
+++ app/entry.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class entry extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+
