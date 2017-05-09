db0826423232635bbb629dc4ceeb73551972b5f5
zouqin <zouqin3306@sina.com>
2017-05-09 17:10:26 +0800

    20170509

--- app/entry.js
+++ app/entry.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../css/test.scss';
 
 console.log( document.getElementById('plan') );
 ReactDOM.render(
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,22 @@
 var path = require("path");           //node path 模块
 var webpack = require('webpack');     //webpack   核心模块
 
+var plug = [];                        //插件数组
+var ExtractTextPlugin = require('extract-text-webpack-plugin'); // 提取文件插件
+
+// 压缩js插件
+var UglifyJsPlugin = new webpack.optimize.UglifyJsPlugin({       
+        output: {
+          comments: false,  // 清楚注释信息
+        },
+        compress: {
+          warnings: false,
+        }
+      });
+plug.push( UglifyJsPlugin );
+
+
+
 module.exports = {
   /**
    * entry:入口文件
@@ -13,7 +29,7 @@ module.exports = {
   },
   module: {
     loaders: [
-    	/** 将css文件加载到<style>标签中 **/
+    	  /** 将css文件加载到<style>标签中 **/
         {test: /\.css$/, loader: 'style'},	
       
         /**支持JS中以  requier() 的形式加载 .css文件**/
@@ -23,7 +39,10 @@ module.exports = {
         {test: /\.(png|jpg)$/,loader:'url?limit=1000'},
         
         /**解析 react和 es6 **/
-        {test: /\.js?$/, loader: 'babel', query: { presets: ['es2015', 'react'] } }
+        {test: /\.js?$/, loader: 'babel', query: { presets: ['es2015', 'react'] } },
+        
+        /**这里用了样式分离出来的插件，如果不想分离出来，可以直接这样写 loader:'style!css!sass'**/
+        {test: /\.scss$/, loader:'style!css!sass' }
     ]
   },
 
@@ -35,17 +54,5 @@ module.exports = {
   },
   
   /**插件**/
-  plugins:[
-		/**
-		 * 压缩js插件
-		 */
-		new webpack.optimize.UglifyJsPlugin({				
-	      output: {
-	        comments: false,  // 清楚注释信息
-	      },
-	      compress: {
-	        warnings: false,
-	      }
-	    })
-	]
+  plugins: plug,
 }
